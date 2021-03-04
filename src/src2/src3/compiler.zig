5acaad7bbf1687777ebaea06807e906b358a53d7
pfg <pfg@pfg.pw>
2021-03-03 17:39:46 -0800

    → "TODO infer type of expression"

--- src/src2/src3/compiler.zig
+++ src/src2/src3/compiler.zig
@@ -56,12 +56,28 @@ pub fn preinitVariables(scope: *Scope, expr: *ast.Expression) RuntimeError!void 
 // () eg (let a = int: 25, a)
 // {} eg <blkname>{let a = int: 25; return<blkname> 25;}
 // <…> eg <blkname>({return <blkname> 25;}) // uuh maybe <blkname> should just be a thing that goes before parens or curlies
-pub fn evaluateExpression(scope: *Scope, expr: *ast.Expression) RuntimeError!*IR {
+pub fn evaluateExpression(scope: *Scope, expr: *ast.Expression) RuntimeError!TypedValue {
     switch (expr.*) {
-        .vardecl => {
+        .vardecl => |vd| {
+            //  return IR.vardecl(.{
+            //      .name = jsSafe(name), // adds a random number to the thing also
+            //      .value = cast(type, evaluateExpression(scope, vd.initv)),
+            //  });
+            const var_initializer = scope.getVariableInitializer(vd.name.*, vd.initv) catch |e| switch (e) {
+                error.NotFound => return displayErrorAt(expr, "variable is not found"),
+            };
+            const var_info = try var_initializer.info();
+
             // get the varinfo for this variable
             // emit const safe(varname) = evaluateExpression(value)
-            return allocDupe(scope.name_map.allocator, IR{ .todo = .{} }) catch @panic("oom");
+            // return allocDupe(scope.name_map.allocator, IR{ .todo = .{} }) catch @panic("oom");
+            return TypedValue{
+                .ir = IR.vardecl(scope.name_map.allocator, .{
+                    .name = IR.VarName{ .name = var_info.name, .id = var_info.id },
+                    .init_expr = try cast(var_info.ty, try evaluateExpression(scope, vd.initv)), // todo disallow var a = var b = 25;
+                }),
+                .ty = Type.new(scope.name_map.allocator, .void),
+            };
         },
         else => {
             std.debug.panic("TODO expression {s}", .{std.meta.tagName(expr.*)});
@@ -69,18 +85,53 @@ pub fn evaluateExpression(scope: *Scope, expr: *ast.Expression) RuntimeError!*IR
     }
 }
 
+// returns *IR of type ty
+pub fn cast(ty: *Type, tval: TypedValue) RuntimeError!*IR {
+    switch (ty.value) {
+        .int => {
+            switch (tval.ty.value) {
+                .int => return tval.ir,
+                .exact_number => return tval.ir,
+                else => return displayErrorAt(tval.ir, "Error cannot cast {std.meta.tagName(tval.ty.value)} → int"),
+            }
+        },
+        else => {
+            std.debug.panic("TODO cast to {s}", .{std.meta.tagName(ty.value)});
+        },
+    }
+}
+
+pub const TypedValue = struct {
+    ty: *Type,
+    ir: *IR,
+};
+
 pub const IR = union(enum) {
+    const VarName = struct {
+        id: usize, // an id that refers to this variable only. var a = (var a = 25, a) → var «1» = (var «2» = 25, «2»)
+        name: []const u8, // decorative but must be the same as all other varnames with the same id
+    };
+    const VarDecl = struct {
+        name: VarName,
+        init_expr: *IR,
+    };
     todo,
     block: struct {
+        // note a seperate JSIR should be used for emission that also keeps track of if expressions need to do
+        // things js expressions can't do eg break
         code: std.ArrayList(*IR),
     },
+    vardecl: VarDecl,
     pub fn newBlock(alloc: *std.mem.Allocator) *IR {
         return allocDupe(alloc, IR{ .block = .{ .code = std.ArrayList(*IR).init(alloc) } }) catch @panic("oom");
+    }
+    pub fn vardecl(alloc: *std.mem.Allocator, vd: VarDecl) *IR {
+        return allocDupe(alloc, IR{ .vardecl = vd }) catch @panic("oom");
     }
 };
 
 const Type = struct {
-    value: union(enum) {
+    const TypeUnion = union(enum) {
         exact_number, // eg 25
         int, // eg int: 25
         function: struct { // eg fn() void: {}
@@ -88,7 +139,13 @@ const Type = struct {
             ret_v: *Type,
         },
         never, // noreturn / ⊥bottom type
-    },
+        @"void", // void / unit type conveying a value that does not need saving
+    };
+    value: TypeUnion,
+
+    pub fn new(alloc: *std.mem.Allocator, v: TypeUnion) *Type {
+        return allocDupe(alloc, Type{ .value = v }) catch @panic("oom");
+    }
 };
 
 // (int: 25) → int
@@ -115,6 +172,13 @@ pub fn typeOfExpression(scope: *Scope, expr: *ast.Expression) RuntimeError!*Type
 
 // const y = fn() {print(x)}
 // var x = 25;
+
+const VariableInfo = struct {
+    ty: *Type,
+    name: []const u8,
+    id: usize,
+};
+
 pub const VariableInitializer = struct {
     value: union(enum) {
         uninitialized: struct {
@@ -122,31 +186,34 @@ pub const VariableInitializer = struct {
             scope: *Scope,
         },
         progress,
-        ready: struct {
-            ty: *Type,
-        },
+        ready: VariableInfo,
     },
-    pub fn getType(vi: *VariableInitializer) RuntimeError!*Type {
+    pub fn info(vi: *VariableInitializer) RuntimeError!VariableInfo {
         return switch (vi.value) {
             .uninitialized => |un_temp| {
                 const un = un_temp;
                 vi.value = .progress;
                 const type_v = try typeOfExpression(un.scope, un.expr);
-                vi.value = .{ .ready = .{ .ty = type_v } };
-                return type_v;
+                const varinfo: VariableInfo = .{
+                    .ty = type_v,
+                    .name = "TODO Name",
+                    .id = getNewID(),
+                };
+                vi.value = .{ .ready = varinfo };
+                return varinfo;
             },
             // eg `const a = b; const b = a;` that will cause this error because the types cannot be resolved
             // `const a = int: b; const b = int: a` is fine though and will just cause a runtime error / possibly a different compile time error.
             .progress => @panic("TODO Error: Loop found"),
 
-            .ready => |rdy| rdy.ty,
+            .ready => |rdy| rdy,
         };
     }
 };
 
 const RuntimeError = error{ErrorAt};
 
-pub fn displayErrorAt(expression: *ast.Expression, msg: []const u8) error{ErrorAt} {
+pub fn displayErrorAt(first_thing: anytype, msg: []const u8) error{ErrorAt} {
     std.log.err("error line x col x: {s}", .{msg});
     return error.ErrorAt;
 }
@@ -189,6 +256,14 @@ pub const Scope = struct {
         if (prev_v) |pv| {
             return error.AlreadyDefined;
         }
+    }
+
+    pub fn getVariableInitializer(scope: *Scope, name: []const u8, expr: *ast.Expression) error{NotFound}!*VariableInitializer {
+        const value = scope.name_map.get(name) orelse {
+            if (scope.parent) |parent| return parent.getVariableInitializer(name, expr);
+            return error.NotFound;
+        };
+        return value;
     }
 };
 
