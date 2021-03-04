ad92bb84383f7cd8144f85ba720485498e2426fe
pfg <pfg@pfg.pw>
2021-03-03 16:56:37 -0800

    uil work

--- src/src2/src3/compiler.zig
+++ src/src2/src3/compiler.zig
@@ -1,7 +1,6 @@
 const ast = @import("uilang_parser");
 const std = @import("std");
 const Alloc = std.mem.Allocator;
-const Type = @import("type.zig").Type;
 usingnamespace @import("../../help.zig");
 
 pub fn main() !void {
@@ -24,8 +23,32 @@ pub fn main() !void {
 
     const ir_block = IR.newBlock(alloc);
     const scope = Scope.new(alloc, null, ir_block, .none);
-    for (parsed) |expr| {
-        _ = evaluateExpression(scope, expr);
+
+    // part 1: find and define all variables
+    // part 2: go through and evaluate
+    for (parsed) |*expr| {
+        try preinitVariables(scope, expr);
+    }
+    for (parsed) |*expr| {
+        _ = try evaluateExpression(scope, expr);
+    }
+}
+
+pub fn preinitVariables(scope: *Scope, expr: *ast.Expression) RuntimeError!void {
+    switch (expr.*) {
+        .vardecl => |vd| {
+            switch (vd.vartype) {
+                .const_ => {},
+                else => {
+                    // todo displayErrorAt(expr.vartype)
+                    return displayErrorAt(expr, "TODO unsupported var type {std.meta.tagName(vd.vartype)}");
+                },
+            }
+            scope.makeVariable(vd.name.*, vd.initv) catch |e| switch (e) {
+                error.AlreadyDefined => return displayErrorAt(expr, "variable is already defined"),
+            };
+        },
+        else => {},
     }
 }
 
@@ -33,17 +56,21 @@ pub fn main() !void {
 // () eg (let a = int: 25, a)
 // {} eg <blkname>{let a = int: 25; return<blkname> 25;}
 // <…> eg <blkname>({return <blkname> 25;}) // uuh maybe <blkname> should just be a thing that goes before parens or curlies
-pub fn evaluateExpression(scope: *Scope, expr: ast.Expression) *IR {
-    switch (expr) {
+pub fn evaluateExpression(scope: *Scope, expr: *ast.Expression) RuntimeError!*IR {
+    switch (expr.*) {
+        .vardecl => {
+            // get the varinfo for this variable
+            // emit const safe(varname) = evaluateExpression(value)
+            return allocDupe(scope.name_map.allocator, IR{ .todo = .{} }) catch @panic("oom");
+        },
         else => {
-            std.debug.panic("TODO expression {s}", .{std.meta.tagName(expr)});
+            std.debug.panic("TODO expression {s}", .{std.meta.tagName(expr.*)});
         },
     }
 }
 
-pub const TypedValue = struct {};
-
 pub const IR = union(enum) {
+    todo,
     block: struct {
         code: std.ArrayList(*IR),
     },
@@ -51,6 +78,78 @@ pub const IR = union(enum) {
         return allocDupe(alloc, IR{ .block = .{ .code = std.ArrayList(*IR).init(alloc) } }) catch @panic("oom");
     }
 };
+
+const Type = struct {
+    value: union(enum) {
+        exact_number, // eg 25
+        int, // eg int: 25
+        function: struct { // eg fn() void: {}
+            args: []*Type,
+            ret_v: *Type,
+        },
+        never, // noreturn / ⊥bottom type
+    },
+};
+
+// (int: 25) → int
+// some_var → typeof(somevar)
+// 25 → 25
+// 5 + 6 → 11
+// (const recurse = fn() void: {return recurse()}, recurse) → fn(): void
+// (const a = return 3) → never (⊥)
+pub fn typeOfExpression(scope: *Scope, expr: *ast.Expression) RuntimeError!*Type {
+    // if expression is type":"expression return type
+    // else @panic("todo infer types")
+    switch (expr.*) {
+        // type: value => evaluateTypeExpression(type)
+        else => {
+            std.debug.panic("TODO infer type of expression .{s}", .{std.meta.tagName(expr.*)});
+        },
+    }
+}
+
+// first, x is added with preinitVariables
+// then, when print(x) is encountered, x is type analyzed to get its type (25)
+// type-analysis does the minimum possible to get the type. `int: …` can exit immediately because it knows the type is "int"
+// when var x is encountered, it is already analyzed and ir just has to be inserted
+
+// const y = fn() {print(x)}
+// var x = 25;
+pub const VariableInitializer = struct {
+    value: union(enum) {
+        uninitialized: struct {
+            expr: *ast.Expression,
+            scope: *Scope,
+        },
+        progress,
+        ready: struct {
+            ty: *Type,
+        },
+    },
+    pub fn getType(vi: *VariableInitializer) RuntimeError!*Type {
+        return switch (vi.value) {
+            .uninitialized => |un_temp| {
+                const un = un_temp;
+                vi.value = .progress;
+                const type_v = try typeOfExpression(un.scope, un.expr);
+                vi.value = .{ .ready = .{ .ty = type_v } };
+                return type_v;
+            },
+            // eg `const a = b; const b = a;` that will cause this error because the types cannot be resolved
+            // `const a = int: b; const b = int: a` is fine though and will just cause a runtime error / possibly a different compile time error.
+            .progress => @panic("TODO Error: Loop found"),
+
+            .ready => |rdy| rdy.ty,
+        };
+    }
+};
+
+const RuntimeError = error{ErrorAt};
+
+pub fn displayErrorAt(expression: *ast.Expression, msg: []const u8) error{ErrorAt} {
+    std.log.err("error line x col x: {s}", .{msg});
+    return error.ErrorAt;
+}
 
 // scopes are created:
 // when any block opens {}
@@ -66,7 +165,7 @@ pub const Scope = struct {
         label: []const u8,
         none,
     };
-    const NameMap = std.StringHashMap(*TypedValue);
+    const NameMap = std.StringHashMap(*VariableInitializer);
     pub fn new(alloc: *std.mem.Allocator, parent: ?*Scope, ir_block: *IR, cf_catch: CFCatch) *Scope {
         var allocated = alloc.create(Scope) catch @panic("oom");
 
@@ -78,6 +177,18 @@ pub const Scope = struct {
         };
 
         return allocated;
+    }
+
+    pub fn makeVariable(scope: *Scope, name: []const u8, expr: *ast.Expression) error{AlreadyDefined}!void {
+        // TODO if already defined, error
+        const vinitializer = allocDupe(
+            scope.name_map.allocator,
+            VariableInitializer{ .value = .{ .uninitialized = .{ .expr = expr, .scope = scope } } },
+        ) catch @panic("oom");
+        const prev_v = scope.name_map.fetchPut(name, vinitializer) catch @panic("oom");
+        if (prev_v) |pv| {
+            return error.AlreadyDefined;
+        }
     }
 };
 
